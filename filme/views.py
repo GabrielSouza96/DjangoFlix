b529eaf9d89ec9d1e3dbba8efada7a933f82b843
GabrielSouza96 <gabriel.souza_01@hotmail.com>
2022-01-14 12:09:41 -0300

    Implementação de um contexto novo. Adicionado recurso de pesquisa, lista de filmes novos, e em alta.

--- filme/views.py
+++ filme/views.py
@@ -18,6 +18,33 @@ class Detalhesfilme(DetailView):
     template_name = "detalhesfilme.html"
     model = Filme
 
+    def get(self, request, *args, **kwargs): #contabilizou uma visualizacao
+        filme = self.get_object()
+        filme.visualizacoes += 1
+        filme.save()
+        return super().get(request, *args, **kwargs) #redireciona para a url final
+
+
+    def get_context_data(self, **kwargs):
+        context = super(Detalhesfilme, self).get_context_data(**kwargs)
+        filmes_relacionados = Filme.objects.filter(categoria=self.get_object().categoria)[0:5]
+        context["filmes_relacionados"] = filmes_relacionados
+        return context
+
+
+class Pesquisafilme(ListView):
+    template_name = "pesquisafilme.html"
+    model = Filme
+
+    #Editando object_list
+    def get_queryset(self):
+        termo_pesquisa = self.request.GET.get('query')
+        if termo_pesquisa:
+            object_list = self.model.objects.filter(titulo__icontains=termo_pesquisa)
+            return object_list
+        else:
+            return None
+
 #Function Base Views
 #def homepage(request):
 #    return render(request, "homepage.html")
