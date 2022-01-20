b348aca5df8c32943166a0acb54ae4dedeb2a368
GabrielSouza96 <gabriel.souza_01@hotmail.com>
2022-01-20 15:58:31 -0300

    Ajuste das urls nos templates, usuário, criação da página de login e logout para questão de teste.

--- filme/views.py
+++ filme/views.py
@@ -1,20 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Filme
 from django.views.generic import TemplateView, ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 #Class Base Views
 
 class Homepage(TemplateView):
     template_name = "homepage.html"
 
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('filme:homefilmes')
+        else:
+            return super().get(request, *args, **kwargs)#redireciona para homepage
 
-
-
-class Homefilmes(ListView):
+class Homefilmes(LoginRequiredMixin, ListView):
     template_name = "homefilmes.html"
     model = Filme
 
-class Detalhesfilme(DetailView):
+
+class Detalhesfilme(LoginRequiredMixin, DetailView):
     template_name = "detalhesfilme.html"
     model = Filme
 
@@ -22,17 +27,21 @@ class Detalhesfilme(DetailView):
         filme = self.get_object()
         filme.visualizacoes += 1
         filme.save()
+        usuario = request.user
+        usuario.filmes_vistos.add(filme) #ao clicar na pagina de detalhes filme, adiciono ao filmes vistos
         return super().get(request, *args, **kwargs) #redireciona para a url final
 
 
     def get_context_data(self, **kwargs):
         context = super(Detalhesfilme, self).get_context_data(**kwargs)
+        #filtrar tabela de filmes pegando filmes cuja categoria é igual a vategoria do filme da página (objeto)
+        # self.get_object()
         filmes_relacionados = Filme.objects.filter(categoria=self.get_object().categoria)[0:5]
         context["filmes_relacionados"] = filmes_relacionados
         return context
 
 
-class Pesquisafilme(ListView):
+class Pesquisafilme(LoginRequiredMixin, ListView):
     template_name = "pesquisafilme.html"
     model = Filme
 
@@ -44,6 +53,10 @@ class Pesquisafilme(ListView):
             return object_list
         else:
             return None
+
+class Paginaperfil(LoginRequiredMixin, TemplateView):
+    template_name = "editarperfil.html"
+
 
 #Function Base Views
 #def homepage(request):
