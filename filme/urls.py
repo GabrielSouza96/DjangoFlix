5d8861b6c600d55871576f728486f5f441ced55d
GabrielSouza96 <gabriel.souza_01@hotmail.com>
2022-01-26 17:42:35 -0300

    Adicionada as funcionalidades de editar perfil, alteração de senha, basicamente parte de usuário.

--- filme/urls.py
+++ filme/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path, include
-from .views import Homepage, Homefilmes, Detalhesfilme, Pesquisafilme, Paginaperfil
+from django.urls import path, reverse_lazy
+from .views import Homepage, Homefilmes, Detalhesfilme, Pesquisafilme, Paginaperfil, Criarconta
 from django.contrib.auth import views as auth_view
+
 app_name = 'filme'
 
 urlpatterns = [
@@ -10,5 +11,9 @@ urlpatterns = [
     path('pesquisa/', Pesquisafilme.as_view(), name='pesquisafilme'),
     path('login/', auth_view.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_view.LogoutView.as_view(template_name='logout.html'), name='logout'),
-    path('editarperfil/', Paginaperfil.as_view(), name='editarperfil'),
+    path('editarperfil/<int:pk>', Paginaperfil.as_view(), name='editarperfil'),
+    path('criarconta/', Criarconta.as_view(), name='criarconta'),
+    path('mudarsenha/', auth_view.PasswordChangeView.as_view(template_name='editarperfil.html',
+                                                             success_url=reverse_lazy('filmes:homefilmes'))
+                                                             , name='mudarsenha'),
 ]
--- filme/views.py
+++ filme/views.py
@@ -1,18 +1,31 @@
-from django.shortcuts import render, redirect
-from .models import Filme
-from django.views.generic import TemplateView, ListView, DetailView
+from django.shortcuts import render, redirect, reverse
+from .models import Filme, Usuario
+from .forms import CriarContaForm, FormHomePage
+from django.views.generic import TemplateView, ListView, DetailView, FormView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 #Class Base Views
 
-class Homepage(TemplateView):
+class Homepage(FormView):
     template_name = "homepage.html"
+    form_class = FormHomePage
 
     def get(self, request, *args, **kwargs):
         if request.user.is_authenticated:
             return redirect('filme:homefilmes')
         else:
             return super().get(request, *args, **kwargs)#redireciona para homepage
+
+    def get_success_url(self):
+        email = self.request.POST.get("email")
+        usuarios = Usuario.objects.filter(email=email)
+        if usuarios:
+            return reverse("filme:login")
+        else:
+            return reverse("filme:criarconta")
+
+
 
 class Homefilmes(LoginRequiredMixin, ListView):
     template_name = "homefilmes.html"
@@ -54,9 +67,24 @@ class Pesquisafilme(LoginRequiredMixin, ListView):
         else:
             return None
 
-class Paginaperfil(LoginRequiredMixin, TemplateView):
+class Paginaperfil(LoginRequiredMixin, UpdateView):
     template_name = "editarperfil.html"
+    model = Usuario
+    fields = ['first_name', 'last_name', 'email']
 
+    def get_success_url(self):
+        return reverse("filme:homefilmes")
+
+class Criarconta(FormView):
+    template_name = "criarconta.html"
+    form_class = CriarContaForm
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('filme:login')#espera um url como retorno da funçao por isso usa-se reverse
 
 #Function Base Views
 #def homepage(request):
